1b64de7a612a19aa8a41ca439631e1f0c637f6d1
NoorMutant <141281308+NoorMutant@users.noreply.github.com>
2025-07-02 23:04:41 +0500

    Update qyery.sql

--- Flask Blog/instance/qyery.sql
+++ Flask Blog/instance/qyery.sql
@@ -1,9 +1,1 @@
--- Update password for user 'admin'
-UPDATE users
-SET password = 'mutant5127'
-WHERE username = 'admin';
 
--- Update password for user 'admin2'
-UPDATE users
-SET password = 'mutant5127'
-WHERE username = 'admin2';
